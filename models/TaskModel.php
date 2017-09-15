d930f0888d518f84ead56e0ec6881915064ad3bd
worm2fed <worm2fed@mail.ru>
2017-09-15 21:22:27 +0300

    Create model for tasks

--- models/TaskModel.php
+++ models/TaskModel.php
@@ -1,15 +1,52 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: worm2fed
- * Date: 15.09.17
- * Time: 20:35
+ * This class provides Task model
  */
 
 namespace models;
 
 
-class TaskModel
-{
+use core\exceptions\ValidationException;
+use Model;
 
+class TaskModel extends Model
+{
+    public function __construct()
+    {
+        // Set fields
+        $this->_fields = array_fill_keys([
+            'task_id', 'status', 'name', 'email', 'text', 'image'
+        ], null);
+        // Specify required fields
+        $this->_required = ['name', 'email', 'text'];
+        // Specify default values
+        $this->status = 0;
+    }
+
+    /**
+     * Get table name
+     *
+     * @return string
+     */
+    public static function getTableName()
+    : string {
+        return 'task';
+    }
+
+    /**
+     * Validate fields
+     */
+    public function validate()
+    : void {
+        // Check whether required fields are set
+        foreach ($this->_required as $required) {
+            if (!isset($this->_fields[$required])) {
+                throw new ValidationException("`$required can not be blank`", E_USER_ERROR);
+            }
+        }
+        // Validate email address
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            throw new ValidationException("`email has wrong format`", E_USER_ERROR);
+        }
+    }
 }
