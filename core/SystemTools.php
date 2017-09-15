8f642e0aa3b8f08a922beaeb3d522c332d57919d
worm2fed <worm2fed@mail.ru>
2017-09-15 17:42:40 +0300

    add class with different tools

--- core/SystemTools.php
+++ core/SystemTools.php
@@ -1,15 +1,48 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: worm2fed
- * Date: 15.09.17
- * Time: 17:16
+ * This class provides different tools
  */
 
 namespace core;
 
 
+use Config;
+
 class SystemTools
 {
+    private function __construct()
+    {
+    }
 
+    /**
+     * Get error type by its level
+     * @param int $level
+     * @return string
+     */
+    public static function get_error_type(int $level)
+    : string {
+        if (array_key_exists($level, Config::$ERROR_LEVELS)) {
+            return Config::$ERROR_LEVELS[$level];
+        } else {
+            return 'E_UNKNOWN';
+        }
+    }
+
+    /**
+     * Generate hash-code
+     *
+     * @param int $length
+     * @return string
+     */
+    public static function generate_hash_code(int $length = 6)
+    : string {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRQSTUVWXYZ0123456789";
+        $code = "";
+        $len = strlen($chars) - 1;
+
+        while (strlen($code) < $length) {
+            $code .= $chars[mt_rand(0, $len)];
+        }
+        return $code;
+    }
 }
