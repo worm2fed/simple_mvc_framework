deaa85ad1de2e3bacf3f35c1517fc9fc3f121ce8
worm2fed <worm2fed@mail.ru>
2017-09-16 01:23:12 +0300

    View class

--- core/View.php
+++ core/View.php
@@ -1,3 +1,33 @@
 <?php
 
 namespace core;
+
+
+use Config;
+use core\exceptions\ViewException;
+
+/**
+ * Class View
+ * @package core
+ */
+class View
+{
+    /**
+     * Render a view
+     *
+     * @param string $view
+     * @param array $data
+     * @return void
+     * @throws ViewException
+     */
+    public static function render(string $view, array $data = [])
+    : void {
+        extract($data, EXTR_SKIP);
+        $file = Config::ROOT_DIR . "/views/$view";
+        if (file_exists($file)) {
+            require $file;
+        } else {
+            throw new ViewException("`View $file was not found`", E_ERROR);
+        }
+    }
+}
