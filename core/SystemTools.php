52722e9051e4b3d6a02f2f7db3c6b0f1d18582f2
worm2fed <worm2fed@mail.ru>
2017-09-16 20:51:09 +0300

    method for transliteration

--- core/SystemTools.php
+++ core/SystemTools.php
@@ -16,6 +16,53 @@ class SystemTools
     }
 
     /**
+     * @var string[] Replace list for translit
+     */
+    private static $_replace = [
+        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd',
+        'е' => 'e', 'ё' => 'e', 'ж' => 'j', 'з' => 'z', 'и' => 'i',
+        'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n',
+        'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
+        'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'ts', 'ч' => 'ch',
+        'ш' => 'sh', 'щ' => 'shh', 'ъ' => '', 'ы' => 'y', 'ь' => '',
+        'э' => 'e', 'ю' => 'u', 'я' => 'ya',
+
+        '-' => '-', ' ' => '-', '.' => '-', ',' => '-',
+    ];
+
+    /**
+     * Tranlit
+     *
+     * @param string $text
+     * @return string
+     */
+    public static function translit(string $text)
+    : string {
+        $text = mb_strtolower($text);
+        // cyrilic and symbols translit
+        $replace = self::$_replace;
+        $s = '';
+        for ($i = 0; $i < mb_strlen($text); $i++) {
+            $c = mb_substr($text, $i, 1);
+            if (array_key_exists($c, $replace)) {
+                $s .= $replace[$c];
+            } else {
+                $s .= $c;
+            }
+        }
+        // other translit
+        // make sure that you set locale for using iconv
+        $s = iconv('UTF-8', 'ASCII//TRANSLIT', $s);
+        // remove symbols
+        $s = preg_replace('/[^\-0-9a-z]+/i', '', $s);
+        // double spaces
+        $s = preg_replace('/\-+/', '-', $s);
+        // spaces at begin and end
+        $s = preg_replace('/^\-*(.*?)\-*$/', '$1', $s);
+        return $s;
+    }
+
+    /**
      * Get error type by its level
      * @param int $level
      * @return string
