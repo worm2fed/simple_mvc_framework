1bb00088cfa00dfb73579041818cf2dcf6ae77d0
worm2fed <worm2fed@mail.ru>
2017-09-16 20:39:03 +0300

    add static files and template

---
+++ static/js/app.js
@@ -0,0 +1,34 @@
+/***********************************************
+ *        Animation Settings
+ ***********************************************/
+function animate(options) {
+    var animationName = "animated " + options.name;
+    var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+    $(options.selector)
+        .addClass(animationName)
+        .one(animationEnd,
+            function(){
+                $(this).removeClass(animationName);
+            }
+        );
+}
+
+$(function() {
+    var $itemActions = $(".item-actions-dropdown");
+
+    $(document).on('click',function(e) {
+        if (!$(e.target).closest('.item-actions-dropdown').length) {
+            $itemActions.removeClass('active');
+        }
+    });
+
+    $('.item-actions-toggle-btn').on('click',function(e){
+        e.preventDefault();
+
+        var $thisActionList = $(this).closest('.item-actions-dropdown');
+
+        $itemActions.not($thisActionList).removeClass('active');
+
+        $thisActionList.toggleClass('active');
+    });
+});
--- templates/main.php
+++ templates/main.php
@@ -1,7 +1,34 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: worm2fed
- * Date: 16.09.17
- * Time: 17:42
- */
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title> ModularAdmin - Free Dashboard Theme | HTML Version </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/static/css/vendor.css">
+    <!-- Theme initialization -->
+    <link rel="stylesheet" id="theme-style" href="/static/css/app.css">
+</head>
+
+<body>
+<div class="main-wrapper">
+    <div class="app" id="app">
+        <header class="header">
+            <div class="header-block header-block-search"><h4><?= Config::APP_NAME ?></h4></div>
+            <div class="header-block header-block-nav">
+                <button type="button" class="btn btn-danger-outline"><i class="fa fa-sign-out icon"></i> Logout</button>
+                <button type="button" class="btn btn-success"><i class="fa fa-sign-in icon"></i> Login</button>
+            </div>
+        </header>
+
+        <article class="content items-list-page">
+            <?php require $__view ?>
+        </article>
+
+    </div>
+</div>
+<script src="/static/js/vendor.js"></script>
+<script src="/static/js/app.js"></script>
+</body>
+</html>
